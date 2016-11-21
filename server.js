629e1ed9e59dcf8ca250947530c798ba4e8847a1
Maxim Ranchev <maximranchev@Mac-mini-Maxim.local>
2016-11-21 11:39:13 +0200

    adding https support

--- server.js
+++ server.js
@@ -4,7 +4,12 @@ var app = express();
 const PORT = process.env.port || 3000;
 
 app.use(function(req, res, next){
+  if(req.headers['x-forwarded-proto'] === 'http'){
     next();
+  }else{
+    res.redirect('http://' + req.hostname + req.url);
+  }
+
 });
 
 app.use(express.static('public'));
